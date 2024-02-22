00fdd76cc3bb5df90f52565fa48ec3a5b935edeb
ashleyka <ashleykamujoma@gmail.com>
2024-02-22 18:00:32 +0200

    adding some new things

--- To-Do-list-App/app.js
+++ To-Do-list-App/app.js
@@ -9,7 +9,7 @@ const _ = require("lodash");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = []; // Define workItems array
+let workItems = []; 
 
 app.set('view engine', 'ejs');
 
@@ -52,6 +52,28 @@ app.post("/work", function(req, res) {
 
   res.redirect("/work");
 });
+
+
+app.get("/books", function(req, res) {
+  res.render("bookSearch");
+});
+
+
+app.post("/books", function(req, res) {
+  const searchTerm = req.body.searchTerm;
+
+  
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`)
+    .then(response => {
+      const books = response.data.items;
+      res.render("bookResults", { books: books });
+    })
+    .catch(error => {
+      console.error("Error fetching book data:", error);
+      res.render("error", { message: "Failed to fetch book data. Please try again later." });
+    });
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
